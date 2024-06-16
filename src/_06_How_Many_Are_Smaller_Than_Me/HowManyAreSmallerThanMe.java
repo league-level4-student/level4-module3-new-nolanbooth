fdaa434e199bafe611d9ce02158421acea94bb43
league <league@wts14-3.attlocal.net>
2024-06-15 17:33:22 -0700

    jajajaja

--- src/_06_How_Many_Are_Smaller_Than_Me/HowManyAreSmallerThanMe.java
+++ src/_06_How_Many_Are_Smaller_Than_Me/HowManyAreSmallerThanMe.java
@@ -1,30 +1,36 @@
 package _06_How_Many_Are_Smaller_Than_Me;
 
+import java.util.HashMap;
+
 import _05_Intro_to_AVL_Trees.AVLNode;
 import _05_Intro_to_AVL_Trees.AVLTree;
 
 public class HowManyAreSmallerThanMe {
 
-    /*
-     * Given an AVLTree filled with the random numbers from 1 to 20, and a
-     * single number within the AVL Tree, figure out how many numbers in the
-     * AVLTree are smaller than the provided number and return the result.
-     * 
-     * You may want to create a helper method to search through the AVLTree and
-     * count iteratively or recursively.
-     */
+	/*
+	 * Given an AVLTree filled with the random numbers from 1 to 20, and a single
+	 * number within the AVL Tree, figure out how many numbers in the AVLTree are
+	 * smaller than the provided number and return the result.
+	 * 
+	 * You may want to create a helper method to search through the AVLTree and
+	 * count iteratively or recursively.
+	 */
 	int count = 0;
-    public int howManyAreSmallerThanMe(AVLTree<Integer> avlTree, int me) {
-    	System.out.println(avlTree.getRoot().getValue());
-    	System.out.println(me);
-    	
-    	//make a thing that goes forward, searches right and left, and if they don't have any more branches, then go backwards. 
-    	//when going backwards, always go the opposite direction from where you came
-    	//if number is smaller, add to count, and return count. 
-    	AVLNode<Integer> node = avlTree.getRoot();
-    	
-    	
-    	
+	HashMap<Integer, Boolean> h = new HashMap<>();
+
+	// true means visited.
+	public int howManyAreSmallerThanMe(AVLTree<Integer> avlTree, int me) {
+		//System.out.println(avlTree.getRoot().getValue());
+		System.out.println("We want numbers smaller than: " + me);
+
+		// make a thing that goes forward, searches right and left, and if they don't
+		// have any more branches, then go backwards.
+		// when going backwards, always go the opposite direction from where you came
+		// if number is smaller, add to count, and return count.
+		AVLNode<Integer> node = avlTree.getRoot();
+
+		thing(node, me, avlTree);
+
 //    	if(node.getValue() == me) { 
 //    		if(node.getLeft() != null) {
 //    			count++;
@@ -35,41 +41,55 @@ public class HowManyAreSmallerThanMe {
 //    		}
 //    		
 //    	}
-    	
-        return count;
+		System.out.println("smaller than me: " + count);
+		return count;
 
-    }
-    public void thing(AVLNode<Integer> node, int me){
-    	
-    		if(node.getValue() == me) { // node is equal to the value we want
-    			if(node.getLeft() == null) {// get left is null, so we gotta go backwards
-    				if(node.getHeight() !=0) {
-    					node.setHeight(node.getHeight()-1);
-        				thing(node, me);
-    				}else {
-    					//do nothing, we know we have reached the end. 
-    				}
-    				
-    				
-    			}else {
-    				count++;
-    				thing(node.getLeft(), me);
-    			}
-    		}else { // node value not equivalent to what we want
-    			if(node.getValue() > me){// node is greater than the value we want, go left until you can't no more
-    				if(node.getLeft() == null) {
-    					//no damn idea what I'm doing but I think it's right. 
-    				}
-    			}else{
-    				
-    			}
-    			 {
-    				
-    			}
-    		
-    			
-    	}
-    	
-    }
+	}
+
+	public void thing(AVLNode<Integer> node, int me, AVLTree<Integer> avlTree) {
+
+		h.put(node.getValue(), true);
+		// setting current node to visited.
+
+		if (node.equals(avlTree.getRoot()) || node.getHeight() == 0) {// node is root
+			if (node.getValue() >= me) { // root's value is greater (or equal), thus set right root to visited, will not
+											// travel down there
+				h.put(node.getRight().getValue(), true);
+
+			}
+			if (node.getLeft() != null && !h.containsKey(node.getLeft().getValue())) { // get left exists and is not on
+																						// the hashmap
+				
+				//h.put(node.getLeft().getValue(), true); // setting left node to visited. (shouldn't be necessary)
+				thing(node.getLeft(), me, avlTree); // recursive.
+
+			}if(node.getRight() != null && !h.containsKey(node.getRight().getValue())) {
+				thing(node.getRight(), me, avlTree);
+				
+			}
+			if (node.getValue() < me) { // gotta count the root if it is smaller than me.
+				count++;
+			}
+			if (node.getLeft() == null) {
+				// terminate this "thing" method or something, maybe do nothing.
+
+			}
+
+		} else { // node not root, meaning height is not 0
+			if(node.getValue() < me) {
+				count++;
+			}
+			
+			if (node.getLeft() != null && !h.containsKey(node.getLeft().getValue())) { // we know left unexplored
+				
+				thing(node.getLeft(), me, avlTree); // recursive
+				
+			}if(node.getRight() != null && !h.containsKey(node.getRight().getValue())) { // right unexplored. 
+				thing(node.getRight(), me, avlTree);
+			}else {
+				node.setHeight(node.getHeight()-1); // kicks back
+			}
+		}
+	}
 
 }
